94d6fd70f557400a2620b31d779e47fd169d3944
Eliazar <eliazar225ninso@gmail.com>
2023-03-04 19:28:55 +0800

    added darkmode and light mode w Breaking Bad Theme

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
 function darkmode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+  let element = document.body;
+  element.classList.toggle("light-mode");
+
+  let toggleTheme = document.querySelector(".toggle-theme");
+  // console.log(toggleTheme)
+  if (toggleTheme.classList.contains(".fa-sun")) {
+    toggleTheme.classList.toggle("fa-sun")
+    toggleTheme.classList.toggle("fa-moon")
+  } else {
+    toggleTheme.classList.toggle("fa-moon")
+    toggleTheme.classList.toggle("fa-sun")
+  }
+
 }
