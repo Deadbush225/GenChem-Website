b3601a65b1694f3aaaa35a0389de07f031b2ebd7
Eliazar <eliazar225ninso@gmail.com>
2023-03-06 16:43:15 +0800

    darkmode seem to stop flickering

--- script.js
+++ script.js
@@ -12,127 +12,161 @@
 // }
 
 let saveCookie = () => {
-  // alert("cookies saved")
-  l("Saving cookies: ")
-  l(settings)
-  Cookies.set(
-    "settings/defaultTheme",
-    fromStrerializeFlag(Number(settings.defaultTheme))
-  );
-  Cookies.set(
-    "settings/currentTheme",
-    fromStrerializeFlag(Number(settings.currentTheme))
-  ); // we interact to the setting as boolean but we save it as integer
+	// alert("cookies saved")
+	l("Saving cookies: ");
+	l(settings);
+	Cookies.set(
+		"settings/defaultTheme",
+		fromStrerializeFlag(Number(settings.defaultTheme))
+	);
+	Cookies.set(
+		"settings/currentTheme",
+		fromStrerializeFlag(Number(settings.currentTheme))
+	); // we interact to the setting as boolean but we save it as integer
 };
 
 // import Cookies from "../node_modules/js-cookie/dist/js.cookie.mjs";
 // let Cookies = require("js-cookie");
 
 function initCookie() {
-  let settings = {};
+	let settings = {};
 
-  // settings.currentTheme = Cookies.get("settings/currentTheme");
-  // settings.defaultTheme = Cookies.get("settings/defaultTheme");
+	// settings.currentTheme = Cookies.get("settings/currentTheme");
+	// settings.defaultTheme = Cookies.get("settings/defaultTheme");
 
-  // if (!settings) {
-  settings = {
-    // problem, the return if doesn't exist and "true" false is == false
-    defaultTheme: toSterializeFlag(
-      Number(Cookies.get("settings/defaultTheme"))
-    ),
-    currentTheme: toSterializeFlag(
-      Number(Cookies.get("settings/currentTheme"))
-    ),
-  };
-  // }
-  l("Getting Cookies: ");
-  l(settings)
-  return settings;
+	// if (!settings) {
+	settings = {
+		// problem, the return if doesn't exist and "true" false is == false
+		defaultTheme: toSterializeFlag(Number(Cookies.get("settings/defaultTheme"))),
+		currentTheme: toSterializeFlag(Number(Cookies.get("settings/currentTheme"))),
+	};
+	// }
+
+	l("Page reloaded -> Getting Cookies: ");
+	l(settings);
+	return settings;
 }
 
 function fromStrerializeFlag(number) {
-  let i;
+	let i;
 
-  if (number == true) {
-    i = 1;
-  } else if (number == false) {
-    i = 0;
-  }
-  return i;
+	if (number == true) {
+		i = 1;
+	} else if (number == false) {
+		i = 0;
+	}
+	return i;
 }
 
 function toSterializeFlag(number) {
-  let i;
-  // l("input ")
-  // l(number)
+	let i;
+	// l("input ")
+	// l(number)
 
-  i = number == 1 || isNaN(number) ? true : false;
+	i = number == 1 || isNaN(number) ? true : false;
 
-  // if (number == 1) {
-  //   l(78)
-  //   i = true
-  // }
-  // if (number == 0) {
-  //   l(79)
-  //   i = false
-  // }
-  // if (isNaN(number)) {
-  //   l(80)
-  //   i = true
-  // }
-  // l("computed val: ")
-  // l(i)
-  return i;
+	// if (number == 1) {
+	//   l(78)
+	//   i = true
+	// }
+	// if (number == 0) {
+	//   l(79)
+	//   i = false
+	// }
+	// if (isNaN(number)) {
+	//   l(80)
+	//   i = true
+	// }
+	// l("computed val: ")
+	// l(i)
+	return i;
 }
 
 function l(message) {
-  console.log(message);
+	console.log(message);
 }
 
 function applyCurrentTheme() {
-  l("ApplyCurrentTheme :" + settings.currentTheme);
-  changeThemeTo(settings.currentTheme);
+	l("ApplyCurrentTheme :" + settings.currentTheme);
+	// setTimeout(changeThemeTo, 1000, settings.currentTheme);
+	chto(!settings.currentTheme);
+	changeButtonThemes(settings.currentTheme);
 }
 
 function toggleTheme() {
-  l("toggleTheme to :" + !settings.currentTheme);
-  changeThemeTo(!settings.currentTheme);
+	l("toggleTheme to :" + !settings.currentTheme);
+	chto(!settings.currentTheme);
+	changeButtonThemes(!settings.currentTheme);
 }
 
-function changeThemeTo(theme) {
-  l("changing from " + settings.currentTheme + " to " + theme);
-  // l("defaultTheme: " + settings.defaultTheme);
+function chto(theme) {
+	l(
+		"first replenish -> changing from " + settings.currentTheme + " to " + theme
+	);
+	// l("defaultTheme: " + settings.defaultTheme);
 
-  //check if needs to change the default color
-  if (settings.currentTheme == theme) {
-    // saveCookie()
-    if (settings.currentTheme == settings.defaultTheme) {
-      return;
-    }
-  }
-  let element = document.body;
-  element.classList.toggle("light-mode");
+	//check if needs to change the default color
+	if (settings.currentTheme == theme) {
+		// saveCookie()
+		if (settings.currentTheme == settings.defaultTheme) {
+			return;
+		}
+	}
 
-  themeButton = document.querySelector(".toggle-theme");
+	let element = document.documentElement;
 
-  if (theme) {
-    l("DarkMode: Changing to " + theme);
-    themeButton.classList.toggle("fa-sun");
-    themeButton.classList.toggle("fa-moon");
-  } else {
-    l("LightMode: Changing to " + theme);
-    themeButton.classList.toggle("fa-moon");
-    themeButton.classList.toggle("fa-sun");
-  }
-  settings.currentTheme = theme;
+	if (theme) {
+		element.classList.toggle("light-mode");
+		element.classList.toggle("dark-mode");
+		l("DarkMode: Changing to " + theme);
+	} else {
+		element.classList.toggle("dark-mode");
+		element.classList.toggle("light-mode");
+		l("LightMode: Changing to " + theme);
+	}
+}
 
-  l(settings);
-  saveCookie();
+function changeButtonThemes(theme) {
+	l("changing from " + settings.currentTheme + " to " + theme);
+	// l("defaultTheme: " + settings.defaultTheme);
+
+	//check if needs to change the default color
+	if (settings.currentTheme == theme) {
+		// saveCookie()
+		if (settings.currentTheme == settings.defaultTheme) {
+			return;
+		}
+	}
+
+	let element = document.documentElement;
+	let themeButton = document.querySelector(".toggle-theme");
+
+	if (theme) {
+		// element.classList.toggle("dark-mode");
+		// element.classList.toggle("light-mode");
+		l("DarkMode: Changing to " + theme);
+		themeButton.classList.toggle("fa-sun");
+		themeButton.classList.toggle("fa-moon");
+	} else {
+		// element.classList.toggle("light-mode");
+		// element.classList.toggle("dark-mode");
+		l("LightMode: Changing to " + theme);
+		themeButton.classList.toggle("fa-moon");
+		themeButton.classList.toggle("fa-sun");
+	}
+	settings.currentTheme = theme;
+
+	l(settings);
+	saveCookie();
 }
 
 let settings;
+settings = initCookie();
+
+chto(settings.currentTheme);
+
 $(document).ready(() => {
-  settings = initCookie();
-  applyCurrentTheme();
+	// applyCurrentTheme();
+	changeButtonThemes(settings.currentTheme);
 });
 // toggleTheme()
