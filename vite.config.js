b581e7dd9a6fb384c66b88daf14bc5eccb96228a
Eliazar Inso <eliazar225ninso@gmail.com>
2023-04-11 10:26:15 +0800

    Revert "[forward]"
    This reverts commit 0e128b6bfa2f364a165fe54b1452eaa4496224e2.

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,37 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-// import htmlTemplate from 'vite-plugin-html-template'
+
+import fs from "fs";
 // import react from '@vitejs/plugin-react'
 // import sass from 'sass'
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "docs");
 
+const htmlFiles = fs
+	.readdirSync(resolve(root, "pages"))
+	.filter((file) => {
+		console.log(file);
+		return file.endsWith(".html");
+	})
+	.map((file, index) => {
+		return ["html_page" + index, resolve(root, "pages", file)];
+	});
+
 export default defineConfig({
-	// plugins: [
-	// htmlTemplate()],
+	// plugins: [htmlTemplate()],
 	base: "./",
 	publicDir: false,
 	root: root,
 
-	css: {
-		preprocessorOptions: {
-			scss: {
-				// implementation: sass,
-				// additionalData: `
-				//     @import "./src/styles/styles.scss";
-				//     @import "./src/styles/_colorPallete.scss";
-				//     @import "./src/styles/_atomicDetails.scss";
-				//     @import "./src/styles/_atomicModel.scss";
-				//     @import "./src/styles/_generals.scss";
-				//     @import "./src/styles/_home.scss";
-				//     @import "./src/styles/_periodicTable.scss";
-				//     @import "./src/styles/_responsive.scss";
-				//     @import "./src/styles/_utils.scss";
-				// `
-			},
-		},
-	},
+	mode: "Development",
+
+	// css: {
+	// 	preprocessorOptions: {
+	// 		scss: {
+	// 		},
+	// 	},
+	// },
 
 	// publicDir: "./src/",
 
@@ -42,7 +42,8 @@ export default defineConfig({
 			input: {
 				main: resolve(root, "script.ts"),
 				index: resolve(root, "index.html"),
-				periodicTable: resolve(root, "pages", "periodictable.html"),
+
+				...Object.fromEntries(htmlFiles),
 			},
 
 			output: {
@@ -54,12 +55,15 @@ export default defineConfig({
 					if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
 						// extType = "assets";
 						return "assets/[name]-[hash][extname]";
-					}
-					if (/css|ts/i.test(extType)) {
+					} else if (/css|ts/i.test(extType)) {
 						return "styles/[name]-[hash][extname]";
+					} else if (/html/i.test(extType)) {
+						return "pages/[name]-[hash][extname]";
 					}
 
-					// return "assets/[name]-[hash][extname]";
+					// return "styles";
+
+					return "assets/[name]-[hash][extname]";
 					// "css" is only one so no need to catch any possible outcome
 				},
 
